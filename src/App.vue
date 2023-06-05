457ff2d894bccb8eae4713d0a269ef89733a4a54
xiaoli <18959261995@163.com>
2023-06-05 11:45:20 +0800

    add dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const show = ref(true)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <van-dialog v-model:show="show" title="标题" show-cancel-button>
+    <img src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-3.jpeg" />
+  </van-dialog>
 </template>
 
 <style scoped>
@@ -21,9 +16,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+
+import path from "path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers:[VantResolver()]
+    })
+  ],
+  resolve:{
+    alias:{// 这里就是需要配置resolve里的别名
+      "@": path.join(__dirname, "./src"), // path记得引入
+    }
+  }
 })
